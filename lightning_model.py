Fix: delete attention mask

lightning_model.py
@@ -26,7 +26,6 @@ class LightningQALSTM(LightningModule):
 
     @staticmethod
     def add_model_specific_args(parent_parser):
-        # add model specific args
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
         parser.add_argument('--max_len',
                             type=int,
@@ -44,13 +43,10 @@ class LightningQALSTM(LightningModule):
                             default=10)
         parser.add_argument('--margin',
                             type=float,
-                            default=2.0)
+                            default=0.2)
         parser.add_argument('--algorithm',
                             type=str,
                             default='levenshtein')
-        parser.add_argument('--dropout_rate',
-                            type=float,
-                            default=0.1)
 
         return parser
 
@@ -102,9 +98,8 @@ class LightningQALSTM(LightningModule):
             neg = negs[:, np.random.randint(self.hparams.neg_size), :]
             neg_sim = self(query, neg)
             loss = self.loss_fn(pos_sim, neg_sim, self.hparams.margin)
-            # if loss.data[0] != 0:
             losses.append(loss)
-            #     break
+
         val_loss = torch.mean(torch.stack(losses, 0))
         return val_loss
 

model/attn.py
@@ -13,7 +13,7 @@ class BadanauAttn(nn.Module):
         self.w_ms = nn.Linear(2 * hidden_size, 2 * hidden_size, bias=False)
         # self.w_ms = nn.Linear(2 * hidden_size, 1)
 
-    def forward(self, query_pool, rep_out, mask_3d):
+    def forward(self, query_pool, rep_out):
         '''
             query_pool: (batch_size, seq_len)
             rep_out: (batch_size, seq_len, hidden_size * 2)
@@ -26,7 +26,6 @@ class BadanauAttn(nn.Module):
         # Attention Score
         # (batch_size, seq_len, hidden_size * 2)
         attn_score = self.w_ms(W_s_h)
-        attn_score.data.masked_fill_(mask_3d == 0, -float('inf'))
 
         # Alignment Vector (Attention Distribution)
         # (batch_size, seq_len, hidden_size * 2)

model/qa_lstm_with_attn.py
@@ -32,11 +32,7 @@ class RetrievalABLSTM(RetrievalLSTM):
 
         # Get attention weights
         # (batch_size, seq_len, hidden_size * 2)
-        mask = torch.ones_like(reply)
-        mask = torch.where(reply!=1, mask, 0)
-        mask_3d = mask.unsqueeze(dim=2).repeat_interleave(repeats=q_pool.size(-1), dim=2)
-
-        attn_weights = self.attn(q_pool, r_out, mask_3d) 
+        attn_weights = self.attn(q_pool, r_out) 
 
         # Get representation of reply 
         # (batch_size, hidden_size * 2)