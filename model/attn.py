ce0c01a6072b73b32cd735ffd84d93abfe5cb59f
Subin Kim <aqaq925120k@naver.com>
2021-11-19 09:35:42 +0900

    Fix: modify size of W_ms

--- model/attn.py
+++ model/attn.py
@@ -10,7 +10,7 @@ class BadanauAttn(nn.Module):
         self.W_h = nn.Linear(2 * hidden_size, 2 * hidden_size, bias=False) 
         self.W_e = nn.Linear(2 * hidden_size, 2 * hidden_size, bias=False) 
 
-        self.w_ms = nn.Linear(2 * hidden_size, 2 * hidden_size, bias=False)
+        self.w_ms = nn.Linear(2 * hidden_size, 1, bias=False)
         # self.w_ms = nn.Linear(2 * hidden_size, 1)
 
     def forward(self, query_pool, rep_out):
@@ -24,11 +24,11 @@ class BadanauAttn(nn.Module):
         W_s_h = torch.tanh(self.W_h(rep_out) + self.W_e(query_pool).unsqueeze(1).expand(-1, seq_len, -1))
 
         # Attention Score
-        # (batch_size, seq_len, hidden_size * 2)
+        # (batch_size, seq_len, 1)
         attn_score = self.w_ms(W_s_h)
 
         # Alignment Vector (Attention Distribution)
-        # (batch_size, seq_len, hidden_size * 2)
+        # (batch_size, seq_len, 1)
         att_weights = F.softmax(attn_score, dim=1)
         
         return att_weights
